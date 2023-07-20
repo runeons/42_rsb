e0f28064d85dd32daf30876b164712bd2ac544f5
Tess SANTONI <tsantoni@student.42.fr>
2023-07-20 19:58:15 +0200

    ex03 - in class

--- ex03/ex03_boolean_evaluation.py
+++ ex03/ex03_boolean_evaluation.py
@@ -8,50 +8,56 @@ class Node:
         self.left = left
         self.right = right
 
-def create_ast(input):
-    booleans = set(['0', '1'])
-    operators = set(['!', '&', '|', '^', '>', '='])
-    stack = []
-    chars = list(input)
+class Ast:
+    def __init__(self, inp):
+        self.input = inp
+        self.booleans = set(['0', '1'])
+        self.operators = set(['!', '&', '|', '^', '>', '='])
+        self.stack = []
+        self.node = None
 
-    for c in chars:
-        if c in booleans:
-            node = Node(int(c))
-            stack.append(node)
-        elif c in operators:
-            right = stack.pop()
-            left = stack.pop()
-            node = Node(c, left, right)
-            stack.append(node)
-        else:
-            raise ValueError(f"{C_RED}Error:{C_RES} undefined character {c} in {input}")
-    return stack.pop()
-
-def evaluate_ast(node):
-    if isinstance(node.value, int):
-        return int(node.value)
+    def create(self):
+        chars = list(self.input)
+        for c in chars:
+            if c in self.booleans:
+                self.node = Node(int(c))
+                self.stack.append(self.node)
+            elif c in self.operators:
+                right = self.stack.pop()
+                left = self.stack.pop()
+                self.node = Node(c, left, right)
+                self.stack.append(self.node)
+            else:
+                raise ValueError(f"{C_RED}Error:{C_RES} undefined character {c} in {input}")
     
-    left_value = evaluate_ast(node.left)
-    right_value = evaluate_ast(node.right)
-    
-    if node.value == '!':
-        return bool(left_value != right_value)
-    elif node.value == '&':
-        return bool(left_value & right_value)
-    elif node.value == '|':
-        return bool(left_value | right_value)
-    elif node.value == '>':
-        return bool((not left_value) | right_value)
-    elif node.value == '=':
-        return bool(left_value == right_value)
+    def evaluate(self, node=None):
+        if node is None:
+            node = self.node
 
+        if isinstance(node.value, int):
+            return int(node.value)
+        
+        l = self.evaluate(node.left)
+        r = self.evaluate(node.right)
+        
+        if node.value == '!':
+            return bool(l != r)
+        elif node.value == '&':
+            return bool(l & r)
+        elif node.value == '|':
+            return bool(l | r)
+        elif node.value == '>':
+            return bool((not l) or r)
+        elif node.value == '=':
+            return bool(l == r)
 
 def main():
     str_inputs = ["10&", "10=", "10|", "11=", "11>", "1011||="]
     for s in str_inputs:
         try:
-            ast = create_ast(s)
-            result = evaluate_ast(ast)
+            ast = Ast(s)
+            ast.create()
+            result = ast.evaluate()
             print(s, " => ", result)
         except ValueError as e:
             print(e)
