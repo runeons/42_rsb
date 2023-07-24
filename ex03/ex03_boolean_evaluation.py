a67b860ed2543ea09b741cf7d59edfd107543474
Tess SANTONI <tsantoni@student.42.fr>
2023-07-24 11:01:13 +0200

    add print tree

--- ex03/ex03_boolean_evaluation.py
+++ ex03/ex03_boolean_evaluation.py
@@ -1,5 +1,7 @@
 C_GREEN = "\033[92m"
 C_RED = "\033[91m"
+C_YELLOW = "\033[34m"
+C_BLUE = "\033[33m"
 C_RES = "\033[0m"
 
 class Node:
@@ -8,13 +10,14 @@ class Node:
         self.left = left
         self.right = right
 
-class Ast:
+class Rpn:
     def __init__(self, inp):
         self.input = inp
         self.booleans = set(['0', '1'])
         self.operators = set(['!', '&', '|', '^', '>', '='])
         self.stack = []
         self.node = None
+        self.create()
 
     def create(self):
         chars = list(self.input)
@@ -30,16 +33,13 @@ class Ast:
             else:
                 raise ValueError(f"{C_RED}Error:{C_RES} undefined character {c} in {input}")
     
-    def evaluate(self, node=None):
+    def compute(self, node=None):
         if node is None:
             node = self.node
-
         if isinstance(node.value, int):
             return int(node.value)
-        
-        l = self.evaluate(node.left)
-        r = self.evaluate(node.right)
-        
+        l = self.compute(node.left)
+        r = self.compute(node.right)
         if node.value == '!':
             return bool(l != r)
         elif node.value == '&':
@@ -51,13 +51,23 @@ class Ast:
         elif node.value == '=':
             return bool(l == r)
 
+    def print(self, depth=15):
+        print(C_BLUE + "Print tree : " + C_YELLOW + self.input, C_RES)
+        self._print_node(self.node, depth)
+
+    def _print_node(self, node, depth):
+        if node is not None:
+            print("  " * depth, C_YELLOW, node.value, C_RES)
+            self._print_node(node.left, depth - 2)
+            self._print_node(node.right, depth + 2)
+
 def main():
     str_inputs = ["10&", "10=", "10|", "11=", "11>", "1011||="]
     for s in str_inputs:
         try:
-            ast = Ast(s)
-            ast.create()
-            result = ast.evaluate()
+            ast = Rpn(s)
+            # ast.print()
+            result = ast.compute()
             print(s, " => ", result)
         except ValueError as e:
             print(e)
