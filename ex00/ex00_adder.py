fdc9c5a5812853fdb8169192bc62acac7dd4bd31
Tess SANTONI <tsantoni@student.42.fr>
2023-08-02 19:18:23 +0200

    complexity ex00 and ex01 OK

--- ex00/ex00_adder.py
+++ ex00/ex00_adder.py
@@ -1,6 +1,10 @@
 import random
+import time
+
 C_GREEN = "\033[92m"
 C_RED = "\033[91m"
+C_YELLOW = "\033[33m"
+C_BLUE = "\033[34m"
 C_RES = "\033[0m"
 
 def adder(x: int, y: int) -> int:
@@ -8,10 +12,27 @@ def adder(x: int, y: int) -> int:
             raise ValueError(f"{C_RED}Error: {C_RES}Unsigned int expected.")
     while (y != 0):
         carry = x & y
-        addition = x ^ y
-        x = addition
+        x = x ^ y
         y = carry << 1
     return x
+
+def check_time_complexity(f):
+    input_sizes = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+    res = []
+    for n in input_sizes:
+        start_time = time.time_ns()
+        f(n, n)
+        end_time = time.time_ns()
+        execution_time = end_time - start_time
+        res.append((n, execution_time))
+    for i in range(0, len(res)):
+        _, prev_time = res[i - 1]
+        n, curr_time = res[i]
+        if prev_time:
+            ratio = curr_time / prev_time
+            print(f"{C_BLUE}Size:{C_RES} {n}{C_BLUE}, Execution time: {C_RES}{curr_time}{C_BLUE}, Ratio: {C_RES}{ratio}")
+        else:
+            print(f"{C_BLUE}Size:{C_RES} {n}{C_BLUE}, Execution time: {C_RES}{curr_time}{C_BLUE}, Ratio: {C_RES}-")
 
 def main():
     tests = [(0, 0), (4294967295, 4294967295)]
@@ -28,6 +49,7 @@ def main():
                 print(f"{C_RED}{(res == val)}: {C_RES}{x} + {y} = {val} != {res}")
         except ValueError as e:
             print(e)
+    # check_time_complexity(adder)
 
 if (__name__ == "__main__"):
     main()
--- ex01/ex01_multiplier.py
+++ ex01/ex01_multiplier.py
@@ -1,13 +1,16 @@
 import random
+import time
+
 C_GREEN = "\033[92m"
 C_RED = "\033[91m"
+C_YELLOW = "\033[33m"
+C_BLUE = "\033[34m"
 C_RES = "\033[0m"
 
 def adder(x: int, y: int) -> int:
     while (y != 0):
         carry = x & y
-        addition = x ^ y
-        x = addition
+        x = x ^ y
         y = carry << 1
     return x
 
@@ -21,6 +24,24 @@ def multiplier(x: int, y: int) -> int:
         x = x << 1
         y = y >> 1
     return res
+
+def check_time_complexity(f):
+    input_sizes = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+    res = []
+    for n in input_sizes:
+        start_time = time.time_ns()
+        f(n, n)
+        end_time = time.time_ns()
+        execution_time = end_time - start_time
+        res.append((n, execution_time))
+    for i in range(0, len(res)):
+        _, prev_time = res[i - 1]
+        n, curr_time = res[i]
+        if prev_time:
+            ratio = curr_time / prev_time
+            print(f"{C_BLUE}Size:{C_RES} {n}{C_BLUE}, Execution time: {C_RES}{curr_time}{C_BLUE}, Ratio: {C_RES}{ratio}")
+        else:
+            print(f"{C_BLUE}Size:{C_RES} {n}{C_BLUE}, Execution time: {C_RES}{curr_time}{C_BLUE}, Ratio: {C_RES}-")
 
 def main():
     tests = [(0, 0), (4294967295, 4294967295)]
@@ -37,6 +58,7 @@ def main():
                 print(f"{C_RED}{(res == val)}: {C_RES}{x} * {y} = {val} != {res}")
         except ValueError as e:
             print(e)
+    # check_time_complexity(multiplier)
 
 if (__name__ == "__main__"):
     main()
